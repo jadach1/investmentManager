50c1fedb1724165a73e40271a9ca2c24e8907425
jadach1 <jacobadach@live.ca>
2022-02-06 19:29:31 +0100

    Refit ascending/descending order for advanced-table

--- src/app/Fundamentals/main-analysis/Display/display-advance/display-advance.component.ts
+++ src/app/Fundamentals/main-analysis/Display/display-advance/display-advance.component.ts
@@ -17,6 +17,8 @@ companyMasterList: CompanyResults[] = [];
 userSelectedList: CompanyResults[] = [];
 categoryMasterList: string[] = [];
 categoriesSelected: string[] = [];
+direction: boolean = true //For either in Descending or Ascending order, true = descending
+viewAs: boolean = true; //For displaying either in $ or %
 yearList: number[] = [];  // Used to display all the years available in DB for company financials
 
 
@@ -59,29 +61,44 @@ yearList: number[] = [];  // Used to display all the years available in DB for c
     console.log(this.userSelectedList)
   }
 
-  /* ADDING OR REMOVING COMPANIES FROM LIST */
+  /********* ADDING OR REMOVING COMPANIES FROM LIST **********/
 
   //Iterate through all members of the userSelectedList to see if company we are addomg already exists
   public addCompany(company: CompanyResults) {
+
     if(this.userSelectedList.length === 0 ){
       this.userSelectedList.push(company)
     } else {
-      // Check to make sure company doesn't already belong to array
-      let flag: Boolean = false;
 
-      this.userSelectedList.forEach(userCmp => {
-        if(userCmp.id === company.id) {
-            flag = true;
-        }
-      })
-      if(flag === false) {
-        this.userSelectedList.push(company)
-        this.msgServe.sendToast("Added " + company.name, "Add Company",1);
-      } else {
-        this.msgServe.sendToast("Company " + company.name + " Already Exists", "Add Company",4);
+      // Check to make sure company doesn't already belong to array
+      try{
+        this.userSelectedList.forEach(userCmp => {
+          if(userCmp.id === company.id) {
+              throw "exists"
+          }
+        })
+      } catch (company) {
+        if (company != "exists") throw company;
+        this.msgServe.sendToast("Company Already Exists", "Add Company",4);
       }
+      
+      //Company doesn't exist and can be added
+        this.userSelectedList.push(company);
+        this.companyMasterList.splice(this.companyMasterList.indexOf(company),1);
+        this.msgServe.sendToast("Added " + company.name, "Add Company",1);
+   
     }
   }
+
+  //Add all companies to userSelectedList and Remove from MasterList
+  public addAllCompanies() {
+      this.companyMasterList.forEach( company => {
+       this.userSelectedList.push(company);    
+    })
+    this.companyMasterList.splice(0);
+    this.msgServe.sendToast("Added All Companies","Add Company",1);
+  }
+
 
   public dropCompany(id: number){
     this.companyMasterList.forEach( company => {
@@ -90,6 +107,15 @@ yearList: number[] = [];  // Used to display all the years available in DB for c
         this.msgServe.sendToast("Removed " + company.name,"Remove Company",2);
       }
     })
+  }
+
+  //Drops all companies from UserSelectedList and places them in MasterList
+  public dropAllCompanies() {
+    this.userSelectedList.forEach( company => {
+        this.companyMasterList.push(company);
+      })
+      this.userSelectedList.splice(0);
+      this.msgServe.sendToast("Removed All Companies" ,"Remove Companies",3);
   }
 
   /********* CATEGORY LIST *********/
@@ -131,23 +157,38 @@ yearList: number[] = [];  // Used to display all the years available in DB for c
    
   }
 
-      /***************************************************************************\
+  /***************************************************************************\
       SORTING DATA 
   \****************************************************************************/
   // Makes sure this list is sorted by Year - Ascending
-  public sortMasterListAsc() {
+  public sortMasterList() {
+    // this.userSelectedList.forEach( company => {
+    //   for(let i = 0; i < company.results.length; i++){
+    //     for(let k = i + 1; k < company.results.length; k++){
+    //       if(company.results[i].year > company.results[k].year){
+    //         let tempCompany    = new Company();
+    //         tempCompany        = company.results[i];
+    //         company.results[i] = company.results[k];
+    //         company.results[k] = tempCompany;
+    //       }
+    //     }
+    //   }
+    // })
     this.userSelectedList.forEach( company => {
-      for(let i = 0; i < company.results.length; i++){
-        for(let k = i + 1; k < company.results.length; k++){
-          if(company.results[i].year > company.results[k].year){
-            let tempCompany    = new Company();
-            tempCompany        = company.results[i];
-            company.results[i] = company.results[k];
-            company.results[k] = tempCompany;
-          }
-        }
+      let tempCompany = new Array<Company>();
+      for(let i = 0;i < company.results.length;i++){
+        tempCompany.push(company.results[company.results.length - 1 - i]); // copy from the back end of the array
       }
+      company.results = tempCompany;
+      console.log(company.results)
     })
+
+    //Flip years around in new array tempYears
+    let tempYears: number[] = [];
+    for(let i = 0;i < this.yearList.length;i++){
+      tempYears.push(this.yearList[this.yearList.length - 1 - i]); //Copy for the tail end of the array 
+    }
+    this.yearList = tempYears; // Reset the yearList array
   }
 
   public sortMasterListDesc() {
@@ -163,5 +204,19 @@ yearList: number[] = [];  // Used to display all the years available in DB for c
         }
       }
     })
+  }
+
+  /***************************************************************************\
+      MISC FUNCTIONS
+  \****************************************************************************/
+
+  public displayAs(flag: boolean) {
+    this.viewAs = flag;
+  }
+
+  public setDirection(flag: boolean) {
+    this.direction = flag;
+    this.sortMasterList();
+    // flag ? this.sortMasterListDesc():this.sortMasterListAsc();
   }
 }
--- src/app/Fundamentals/main-analysis/Display/display-advance/display-advanced-table/display-advanced-table.component.ts
+++ src/app/Fundamentals/main-analysis/Display/display-advance/display-advanced-table/display-advanced-table.component.ts
@@ -12,40 +12,45 @@ export class DisplayAdvancedTableComponent implements OnInit {
   @Input() valueSelected: boolean = true;  //regarding $ or %
   @Input() categorySelected: string = "";
   @Input() categoriesSelected: string[] = [];
+
+  @Input() displayedAs: boolean = true; //True for $ and false for %
+  @Input() direction: boolean = true;// true for Descending
+
   @Input() yearList: number[] = [];
       yearMin: number;
       yearMax: number;
   
-  hide = new EventEmitter<boolean>();
+  // hide = new EventEmitter<boolean>();
 
   constructor() { }
 
   ngOnInit(): void {
     console.log("Inside")
+    console.log(this.displayedAs)
   }
 
-  public hideTable(){
-    this.hide.emit(true);
-  }
+  // public hideTable(){
+  //   this.hide.emit(true);
+  // }
 
   //Checks to see if a Category exists
-  public isCategoryViewable(): boolean {
-    let exists: boolean = false;
-    try {
-      this.categoriesSelected.forEach( category => {
-        if (category === this.categorySelected) {
-          throw "category"
-        }
-      })
-    } catch (category) {
-      if(category != "category") {
-        throw category;
-      }
-      console.log("caught " + category)
-      return true;
-    }
-    return exists;
-  }
+  // public isCategoryViewable(): boolean {
+  //   let exists: boolean = false;
+  //   try {
+  //     this.categoriesSelected.forEach( category => {
+  //       if (category === this.categorySelected) {
+  //         throw "category"
+  //       }
+  //     })
+  //   } catch (category) {
+  //     if(category != "category") {
+  //       throw category;
+  //     }
+  //     console.log("caught " + category)
+  //     return true;
+  //   }
+  //   return exists;
+  // }
   /***********************= FUNCTIONS TO DISPLAY DATA IN TABLES =***********************/
 
   /*RETURNS ALL THE YEARS STORED IN THE COMPANY */
@@ -68,47 +73,70 @@ export class DisplayAdvancedTableComponent implements OnInit {
   public fetchCategoryValue(companyName: string): number[] | void {
     let tempArray = new Array<number>();
     try{
-        //Go through each company
-        this.listOfCompanies.forEach( company => {
+           //Go through each company
+          this.listOfCompanies.forEach( company => {
 
             //find the company we want to extract data from
             if ( company.name === companyName) {
 
-                  //Sets a marker for which year we are comparing
-                  let  marker = 0;  
+              //Check to see what year this companies newest earnings are reported
+             for(let i = 0; i < this.yearList.length; i++){
 
-                  //Get each value based on desired category
-                  company.results.forEach(period => {     
+              //Check to see which year matches with the latest results
+              if(this.yearList[i] == company.results[0].year) {
+                 console.log("found matching year " + this.yearList[i]);
+                 break;
+               } else { tempArray.push(0)}
+             }
 
-                      for(let i = marker; i < this.yearList.length; i++){
+              //Get each value based on desired category
+              company.results.forEach(period => {     
+                      tempArray.push(period.data.get(this.categorySelected));                
+                  })
 
-                        console.log(marker + " " + period.year + ": " + this.yearList[i])
-                        if(period.year == this.yearList[i]){
-                          tempArray.push(period.data.get(this.categorySelected));
-                          marker = i++;
-                          console.log("g")
-                          break;
-                        } else {
-                          tempArray.push(0);
-                        }
-                      }
-                      console.log("end")
+              // End the loop here, because we have the company we want
+              throw "array"
 
-                  }) // for each company.result
-
-                  throw "array";
-              } // if 
-        }
-        
-        ) // forEach
+            }  //If   
+          })// forEach
     } catch (array) {
       if (array != 'array') throw array;
-      console.log("caught error " + array);
+      //Check to see if we want to display Data in $ or %
+      if(!this.displayedAs){
+        tempArray = this.convertToPercent(tempArray);
+      }
     }
       return tempArray;
   }
 
-  
+  //Converts from $ to %
+  private convertToPercent(baseData: number[]): number[]{
+    //assume we are in desending, so direciton is true
+    let tempArray: number[] = [];
+
+    if(this.direction){
+      //Descending Order
+      for(let i =  0;i < baseData.length - 1;i++){
+        if(baseData[i] != 0 ){
+          tempArray.push( (baseData[i] - baseData[i+1] ) / baseData[i + 1] * 100);
+        }
+      }
+      //Last year doesn't have anything to compare to, so is null.
+      tempArray.push(0);
+    } else {
+      //Ascending Order
+      //Last year doesn't have anything to compare to, so is null.
+      tempArray.push(0);
+      for(let i =  0;i < baseData.length - 1;i++){
+        if(baseData[i] != 0 ){
+          tempArray.push( (baseData[i + 1] - baseData[i] ) / baseData[i] * 100);
+        }
+      }
+      
+    }
+    
+    return tempArray;
+  }
 
   public execute(){
     this.listOfCompanies.splice(0)
